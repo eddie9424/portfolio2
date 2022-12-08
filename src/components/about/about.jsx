55abda5fc15dbe82680146ed3670feeba80808dd
eddie9424 <abdellah.moadine.etu@univ-lille.fr>
2022-12-08 14:47:41 +0100

    mod

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -38,23 +38,35 @@ const about = () => {
 
             </article> */}
           </div>
+          {/* <p>
+            Je suis un étudiant en Master 2 Méthodes quantitatives et modélisation pour l’entreprise.
+          </p> */}
           <p>
-          Salut ! Je m'appelle Abdellah et je suis étudiant à l’université de Lille. Je suis très passionné par les technologies en constante évolution, notamment dans la DATA science et business intelligence.
-          Je crois que les compétences sont la capacité à bien effectuer certaines tâches.
-           Certaines compétences peuvent être mesurées et nous les acquérons par un effort délibéré,
-            d'autres sont liées à nos traits de personnalité. Mes compétences techniques sont les
-             compétences que j'ai acquises grâce à un effort délibéré. Ils peuvent être appris, 
-             enseignés et mesurés, Cela incluent Python, SQL, Microsoft Power BI. Mes compétences non techniques,
-              en revanche incluent , la communication et les relations interpersonnelles, la résolution de problèmes,
-               la négociation, le multitâche, la gestion du temps, la présentation et autres.
+
+
+            Passionné par BIG DATA et attiré par la valeur qu'elles peuvent apportées, je cherche un stage de fin d’études.
+
+            J’ai une forte appétence pour la BIG DATA, Une très bonnes capacités d'analyses de données, et possédant de très bonnes compétences en Business Intelligence, Buisness Analysis, Data modeling, ETL/OLAP/OLTP, algorithmes ML et mathématiques.
+            Je crois que l'apprentissage par la pratique est la meilleure méthodologie d'apprentissage, associée à la collaboration et au travail d'équipe. C'est pourquoi Je suis toujours intrigué et à l'affût de nouvelles technologies, et de nouveaux articles de recherche dans les domaines susmentionnés pour relever un nouveau défi.
+
+
+
+                          
               
 
           </p>
           <p>
+            
+            N'hésitez pas à me contacter, je suis prêt à relever de nouveaux défis et challenges.
+                          
+              
+
+          </p>
+          {/* <p>
           Pendant mon temps libre, j'aime regarder la décomposition des plans vfx et tout ce qui concerne les technologies vfx dans le monde. Je me réjouis particulièrement et m'inspire en regardant des individus 
                talentueux améliorer leur art au moyen de la technologie tout en étant limités par le temps et les budgets.
 
-          </p>
+          </p> */}
           {/* <a href="#contact" className='btn btn-primary'></a> */}
 
 
